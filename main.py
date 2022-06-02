57dd3f81bff907992e50bfa98ba48c9546a80b6f
aomntw <nuntawat.thsrj@gmail.com>
2022-06-02 14:08:26 +0700

    update

--- main.py
+++ main.py
@@ -36,8 +36,11 @@ app = FastAPI(
     description="Chat Backend API",
     version="0.1.0"
 )
+
+
 from app.module_login import login
 app.include_router(login.router)
+
 
 @app.get("/")
 def read_root():
