7e9c60da5f938fef2154d587df4d6b87ec6ba7dc
aomntw <nuntawat.thsrj@gmail.com>
2022-06-02 16:21:45 +0700

    update

--- appfile/main.py
+++ appfile/main.py
@@ -36,7 +36,7 @@ app = FastAPI(
     description="Chat Backend API",
     version="0.1.0"
 )
-from app.module_login import login
+from appfile.module_login import login
 app.include_router(login.router)
 
 
