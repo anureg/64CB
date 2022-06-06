08b263a89282b01d202844edc9134a76166cd4b8
ANU-NUC-M15 <anusird@gmail.com>
2022-06-06 12:58:42 +0700

    A

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Worlddddddd"}
+    return {"Hello": "World (ExDev)"}
 
 
 @app.post("/conversation/unit", tags=["Conversation Unit"])
