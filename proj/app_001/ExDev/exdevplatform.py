77632a3c989f274c900cc50749f0af17736d57eb
aomntw <nuntawat.thsrj@gmail.com>
2022-06-10 10:39:06 +0700

    update platform

--- proj/app_001/ExDev/exdevplatform.py
+++ proj/app_001/ExDev/exdevplatform.py
@@ -6,6 +6,12 @@ from typing import List, Union
 from datetime import datetime, timezone, timedelta
 
 
+class Login_in(BaseModel):
+    username: str
+    password: str
+    device_id: str
+
+
 router = APIRouter(
     prefix="/ExDevPlatform",
     tags=["ExDevPlatform"],
@@ -13,5 +19,23 @@ router = APIRouter(
 
 
 @router.get("/")
-def hello():
-    return {"hello": "platform"}
+def login_platform(login: Login_in):
+    # Time zone in Thailand UTC+7
+    tz = timezone(timedelta(hours=7))
+    # Create a date object with given timezone
+    timestr = datetime.now(tz=tz).isoformat(sep=" ")
+    # hash(device_id + timestr)
+    token = hash(login.device_id + timestr)  # type token int
+
+    username = login.username
+    name = 'Face'
+    surname = 'book'
+    icon = '/ExDev/icon_platform/Facebook.png'
+
+    return {
+        token: token,
+        username: username,
+        name: name,
+        surname: surname,
+        icon: icon
+    }
