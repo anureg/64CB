3e487304af5a44dbc3e6dc25eb394e1e369a263f
aomntw <nuntawat.thsrj@gmail.com>
2022-06-14 11:05:33 +0700

    update login

--- proj/app_001/connection.py
+++
@@ -1,2 +0,0 @@
-from pymongo import MongoClient
-conn = MongoClient("mongodb://root:example@mongo:27017")
---
+++ proj/app_001/platform/exdevplatform.py
@@ -0,0 +1,77 @@
+from fastapi import APIRouter
+
+from pydantic import BaseModel
+from typing import List, Union
+
+from datetime import datetime, timezone, timedelta
+
+
+class Login_in(BaseModel):
+    username: str
+    password: str
+    device_id: str
+
+
+router = APIRouter(
+    prefix="/ExDevPlatform",
+    tags=["ExDevPlatform"],
+)
+
+# post
+path_ImageProfile = "/platform/icon_users/"
+fake_db_users = [
+    {
+        "_id": "6219bc9ba8a68a763a9ae90e",
+        "user_Username": "uname1",
+        "user_Password": "pass1",
+        "user_Name": "name1",
+        "user_Surname": "นามสกุล1",
+        "user_ImageProfile": "6219bc9ba8a68a763a9ae90e.png",
+        "user_Email": "Name1@gmail.com"
+    },
+    {
+        "_id": "6219bccda8a68a763a9ae90f",
+        "user_Username": "uname2",
+        "user_Password": "pass2",
+        "user_Name": "name2",
+        "user_Surname": "นามสกุล2",
+        "user_ImageProfile": "6219bccda8a68a763a9ae90f.png",
+        "user_Email": "Name2@gmail.com"
+    },
+    {
+        "_id": "6219bd1ea8a68a763a9ae910",
+        "user_Username": "uname3",
+        "user_Password": "pass3",
+        "user_Name": "name3",
+        "user_Surname": "นามสกุล3",
+        "user_ImageProfile": "6219bd1ea8a68a763a9ae910.png",
+        "user_Email": "Name3@gmail.com"
+    }
+]
+
+
+class login_req(BaseModel):
+    username: str
+    password: str
+    device_id: str
+
+
+class login_res(BaseModel):
+    token: int
+    _id: str
+    user_Username: str
+    user_Name: str
+    user_Surname: str
+    user_ImageProfile: str
+
+
+@router.post("/", response_model=login_res)
+def login_platform(login: login_req):
+    # Time zone in Thailand UTC+7
+    tz = timezone(timedelta(hours=7))
+    # Create a date object with given timezone
+    timestr = datetime.now(tz=tz).isoformat(sep=" ")
+    # hash(device_id + timestr)
+    token = hash(device_id + timestr)  # type token int
+
+    return login
