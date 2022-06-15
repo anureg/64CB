2fbaf50c1cc89b74a3f657801b94c4cc2f0794e6
aomntw <nuntawat.thsrj@gmail.com>
2022-06-15 10:52:49 +0700

    update db

--- proj/app_001/platform/exdevplatform.py
+++ proj/app_001/platform/exdevplatform.py
@@ -90,3 +90,14 @@ def login_platform(login: login_req):
             }
 
     return {"StatusLogin": "Login error"}
+
+
+
+from pymongo import MongoClient
+conn = MongoClient("mongodb://root:example@10.59.201.28:27017")
+
+mydb = conn["RD_Platform"]
+mycol = mydb["Users"]
+
+x = mycol.find()
+print(x)
