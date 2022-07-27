ad1d5dd6e9bb954d9cae7de2ce9867062c950332
aomntw <nuntawat.thsrj@gmail.com>
2022-07-27 17:23:58 +0700

    x

--- proj/app_001/platform/exdevplatform.py
+++ proj/app_001/platform/exdevplatform.py
@@ -66,21 +66,33 @@ async def login_platform(login: login_req):
 
     fake_db_users = load_json("fake_db_users.json")
     for item in fake_db_users:
-
-        if not item:
-            raise HTTPException(status_code=404, detail="Not found")
-        elif (
-            login.login_Username != item["user_Username"]
-            or login.login_Password != item["user_Password"]
-        ):
-            raise HTTPException(status_code=404, detail="error")
+        if login.login_Username == item["user_Username"]:
+            if (
+                login.login_Username != item["user_Username"]
+                or login.login_Password != item["user_Password"]
+            ):
+                raise HTTPException(status_code=404, detail="error")
+            else:
+                user = item
+                user["login_Status"] = "Success"
+                user["user_id"] = str(user["_id"])
+                user["login_Token"] = create_token(id=login.login_UUID)
         else:
-            user = item
-            user["login_Status"] = "Success"
-            user["user_id"] = str(user["_id"])
-            user["login_Token"] = create_token(id=login.login_UUID)
+            raise HTTPException(status_code=404, detail="Not found")
 
-        return user
+        # if not user:
+        #     raise HTTPException(status_code=404, detail="Not found")
+        # elif (
+        #     login.login_Username != user["user_Username"]
+        #     or login.login_Password != user["user_Password"]
+        # ):
+        #     raise HTTPException(status_code=404, detail="error")
+        # else:
+        #     user["login_Status"] = "Success"
+        #     user["user_id"] = str(user["_id"])
+        #     user["login_Token"] = create_token(id=login.login_UUID)
+
+    return user
 
 
 @router.get("/GetIcon")
